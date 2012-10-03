206375860f0e8e98654d8b90b1535561aa21dfc6
Sylvain Boureliou <sylvain.boureliou@gmail.com>
2012-10-03 14:46:32 +0200

    Move source code to src directory

--- setup.py
+++ setup.py
@@ -3,20 +3,31 @@ from setuptools import setup, find_packages
 
 version = '1.1.dev0'
 
-tests_require = ['zope.testing', 'plone.app.testing']
+def read(*rnames):
+    return open(
+        os.path.join('.', *rnames)
+    ).read()
+
+long_description = "\n\n".join(
+    [read('README.rst'),
+     read('docs', 'HISTORY.txt'),
+    ]
+)
+
+tests_require = ['zope.testing',
+                 'zope.app.testing',
+                 'plone.app.testing',
+                 'lxml']
+
 
 setup(name='plonetheme.drupal',
       version=version,
       description="Get all the power of Drupal for Plone ;)",
-      long_description=open("README.rst").read() + "\n" +
-                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      long_description=long_description,
       # Get more strings from
       # http://pypi.python.org/pypi?:action=list_classifiers
       classifiers=[
         "Framework :: Plone",
-        "Framework :: Zope2",
-        "Framework :: Zope3",
-        "Framework :: Plone :: 4.0",
         "Framework :: Plone :: 4.1",
         "Framework :: Plone :: 4.2",
         "Programming Language :: Python",
@@ -27,7 +38,8 @@ setup(name='plonetheme.drupal',
       author_email='sylvain.boureliou@gmail.com',
       url='https://github.com/sylvainb/plonetheme.drupal',
       license='GPL',
-      packages=find_packages(exclude=['ez_setup']),
+      packages=find_packages('src'),
+      package_dir={'': 'src'},
       namespace_packages=['plonetheme'],
       include_package_data=True,
       zip_safe=False,
@@ -38,8 +50,10 @@ setup(name='plonetheme.drupal',
           # -*- Extra requirements: -*-
       ],
       tests_require=tests_require,
-      extras_require={'test': tests_require},
-      #test_suite='plonetheme.drupal.tests.test_doctest.test_suite',
+      test_suite='plonetheme.drupal.tests.test_doctest.test_suite',
+      extras_require={
+        'test': tests_require,
+      },
       entry_points={
           'z3c.autoinclude.plugin': 'target = plone',
       },
